918bc1fcf3d8c9575022e62ec651af9965ce6ad2
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-07-14 15:39:14 +0530

    Use production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,7 +7,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   withCredentials: true, // sends httpOnly cookies on every request
   headers: { 'Content-Type': 'application/json' },
 });
